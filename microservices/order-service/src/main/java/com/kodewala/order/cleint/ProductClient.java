0f152a41951e402db02387c1efed8eb2f587e54e
kodewala-academy <kodewala.academy@gmail.com>
2025-07-03 14:15:52 +0530

    updated microservice and zipkin fix

---
+++ microservices/order-service/src/main/java/com/kodewala/order/cleint/ProductClient.java
@@ -0,0 +1,14 @@
+package com.kodewala.order.cleint;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.kodewala.order.config.TracingFeignConfig;
+import com.kodewala.order.model.Product;
+
+@FeignClient(name = "product-service" , configuration = TracingFeignConfig.class)
+public interface ProductClient {
+	@GetMapping("/products/{id}")
+	Product getProductById(@PathVariable("id") Long id);
+}
